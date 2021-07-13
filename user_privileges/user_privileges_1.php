f9286f5b9e62f8e063614c689c9360e01ebfc9b4
Nginx Nginx Magento web server <nginx@sandbox.talecsystem.com>
2021-07-13 08:51:13 +0100

    Primer commit

---
+++ user_privileges/user_privileges_1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//This is the access privilege file
+$is_admin=true;
+
+$user_info=array('user_name'=>'admin','is_admin'=>'on','user_password'=>'$2y$10$0MV6D4d3g99/82KORQlsCuO5pE.55FVqScBsAGC3PdVAa16DvNjv2','confirm_password'=>'$2y$10$hNL/iXjwVXmRVD0gr5Y6ROCsnBGRcVoJ3wvhtNdmjHh9FoCAagQ2q','first_name'=>'Ruben','last_name'=>'Gonzalez','roleid'=>'H2','email1'=>'rgonzalez@civicos.com','status'=>'Active','activity_view'=>'This Week','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'23:00','start_hour'=>'00:00','is_owner'=>'1','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'etrtYpzoQExjbh0','time_zone'=>'Europe/London','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'1','theme'=>'alphagrey','language'=>'es_es','reminder_interval'=>'1 Minute','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'1','dayoftheweek'=>'Sunday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'','defaulteventstatus'=>'Planned','defaultactivitytype'=>'Call','hidecompletedevents'=>'0','defaultcalendarview'=>'MyCalendar','defaultlandingpage'=>'Home','currency_name'=>'Euro','currency_code'=>'EUR','currency_symbol'=>'&#8364;','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','id'=>'1');
+
+?>
